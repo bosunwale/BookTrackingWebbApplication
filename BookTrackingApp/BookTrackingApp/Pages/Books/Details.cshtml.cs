40969abbf4933a728bd9a790798146c5935e61d7
johnthebeloved <johnoluwale1@gmail.com>
2022-08-17 19:40:52 -0400

    Razor Pages Using Entity Framework (CRUD)

---
+++ BookTrackingApp/BookTrackingApp/Pages/Books/Details.cshtml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using BookTrackingApp.Data;
+using BookTrackingApp.Models;
+
+namespace BookTrackingApp.Pages.Books
+{
+    public class DetailsModel : PageModel
+    {
+        private readonly BookTrackingApp.Data.BookTrackingAppContext _context;
+
+        public DetailsModel(BookTrackingApp.Data.BookTrackingAppContext context)
+        {
+            _context = context;
+        }
+
+      public Book Book { get; set; } = default!; 
+
+        public async Task<IActionResult> OnGetAsync(int? id)
+        {
+            if (id == null || _context.Book == null)
+            {
+                return NotFound();
+            }
+
+            var book = await _context.Book.FirstOrDefaultAsync(m => m.Id == id);
+            if (book == null)
+            {
+                return NotFound();
+            }
+            else 
+            {
+                Book = book;
+            }
+            return Page();
+        }
+    }
+}
--- BookTrackingApp/BookTrackingApp/Program.cs
+++ BookTrackingApp/BookTrackingApp/Program.cs
@@ -1,7 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using BookTrackingApp.Data;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorPages();
+builder.Services.AddDbContext<BookTrackingAppContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("BookTrackingAppContext") ?? throw new InvalidOperationException("Connection string 'BookTrackingAppContext' not found.")));
 
 var app = builder.Build();
 
