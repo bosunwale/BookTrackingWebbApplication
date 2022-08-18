d404f08d34cc52b067dd3cf4e81b350d180849d5
johnthebeloved <johnoluwale1@gmail.com>
2022-08-17 20:23:12 -0400

    Create Book.cs

---
+++ TrackingBookApplication/TrackingBookApplication/Models/Book.cs
@@ -0,0 +1,13 @@
+﻿namespace TrackingBookApplication.Models
+{
+    public class Book
+    {
+        public int BookId { get; set; }
+        public string ISBN { get; set; }
+        public string Title { get; set; }
+        public Category Category { get; set; }
+        public string Author { get; set; }
+        public List<Category> Categories { get; set; }
+
+    }
+}
