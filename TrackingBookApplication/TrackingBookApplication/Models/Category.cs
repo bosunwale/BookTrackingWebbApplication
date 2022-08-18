56250f339943848d2eac0dbdb2b6fb082496dce0
Johnthebeloved <johnoluwale1@gmail.com>
2022-08-18 05:14:04 +0400

    Delete TrackingBookApplication directory

--- TrackingBookApplication/TrackingBookApplication/Models/Category.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace TrackingBookApplication.Models
-{
-    public class Category
-    {
-        public string NameToken { get; set; }
-        public int CategoryId { get; set; }
-        public string CategoryDescription { get; set; }
-    }
-}
