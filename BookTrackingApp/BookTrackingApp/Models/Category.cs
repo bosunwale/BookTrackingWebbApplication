b195fe2b6f3ba82e7a1393b5fa8429f743a3662c
johnthebeloved <johnoluwale1@gmail.com>
2022-08-17 21:05:04 -0400

    Create Category.cs

---
+++ BookTrackingApp/BookTrackingApp/Models/Category.cs
@@ -0,0 +1,11 @@
+﻿namespace BookTrackingApp.Models
+{
+    public class Category
+    {
+        public string NameToken { get; set; }
+        public int CategoryId { get; set; }
+        public string CategoryDescription { get; set; }
+        public int BookId { get; set; }
+        public Book Book { get; set; }
+    }
+}
