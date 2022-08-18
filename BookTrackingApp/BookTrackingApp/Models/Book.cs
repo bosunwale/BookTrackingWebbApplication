5c0c4555b2d67dbae78977112d8ae989a4d9eab0
johnthebeloved <johnoluwale1@gmail.com>
2022-08-17 21:04:43 -0400

    Update Book.cs

--- BookTrackingApp/BookTrackingApp/Models/Book.cs
+++ BookTrackingApp/BookTrackingApp/Models/Book.cs
@@ -5,12 +5,11 @@ namespace BookTrackingApp.Models
     public class Book
     {
         //properties for the book class
-        public int Id { get; set; }
-        public string BookTitle { get; set; }
-        public string BookAuthor { get; set; }
+        public int BookId { get; set; }
+        public string ISBN { get; set; }
+        public string Title { get; set; }
+        public Category Category { get; set; }
+        public string Author { get; set; }
 
-        //Constraint for rating
-        [Range(1, 10, ErrorMessage = "Rating is between 1 and 10")]
-        public int BookRating { get; set; }
     }
 }
