33accab7f1ba1867706f7a96368ce92f7b2b7c6a
johnthebeloved <johnoluwale1@gmail.com>
2022-08-17 20:22:52 -0400

    Initial Commit

---
+++ BookTrackingApp/BookTrackingApp/Migrations/20220817234645_InitialCreate.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BookTrackingApp.Migrations
+{
+    public partial class InitialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Book",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    BookTitle = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    BookAuthor = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    BookRating = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Book", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Book");
+        }
+    }
+}
---
+++ TrackingBookApplication/TrackingBookApplication/Data/TrackingBookApplicationContext.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using TrackingBookApplication.Models;
+
+namespace TrackingBookApplication.Data
+{
+    public class TrackingBookApplicationContext : DbContext
+    {
+        public TrackingBookApplicationContext (DbContextOptions<TrackingBookApplicationContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<TrackingBookApplication.Models.Book> Book { get; set; } = default!;
+
+        public DbSet<TrackingBookApplication.Models.Category>? Category { get; set; }
+    }
+}
