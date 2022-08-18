37c963b8e1b3e803d9e40abf14846a4beca62b1d
johnthebeloved <johnoluwale1@gmail.com>
2022-08-17 21:04:39 -0400

    Update BookTrackingAppContextModelSnapshot.cs

--- BookTrackingApp/BookTrackingApp/Migrations/BookTrackingAppContextModelSnapshot.cs
+++ BookTrackingApp/BookTrackingApp/Migrations/BookTrackingAppContextModelSnapshot.cs
@@ -23,26 +23,92 @@ namespace BookTrackingApp.Migrations
 
             modelBuilder.Entity("BookTrackingApp.Models.Book", b =>
                 {
+                    b.Property<int>("BookId")
+                        .ValueGeneratedOnAdd()
+                        .HasColumnType("int");
+
+                    SqlServerPropertyBuilderExtensions.UseIdentityColumn(b.Property<int>("BookId"), 1L, 1);
+
+                    b.Property<string>("Author")
+                        .IsRequired()
+                        .HasColumnType("nvarchar(max)");
+
+                    b.Property<string>("ISBN")
+                        .IsRequired()
+                        .HasColumnType("nvarchar(max)");
+
+                    b.Property<string>("Title")
+                        .IsRequired()
+                        .HasColumnType("nvarchar(max)");
+
+                    b.HasKey("BookId");
+
+                    b.ToTable("Book");
+                });
+
+            modelBuilder.Entity("BookTrackingApp.Models.Category", b =>
+                {
+                    b.Property<int>("CategoryId")
+                        .ValueGeneratedOnAdd()
+                        .HasColumnType("int");
+
+                    SqlServerPropertyBuilderExtensions.UseIdentityColumn(b.Property<int>("CategoryId"), 1L, 1);
+
+                    b.Property<int>("BookId")
+                        .HasColumnType("int");
+
+                    b.Property<string>("CategoryDescription")
+                        .IsRequired()
+                        .HasColumnType("nvarchar(max)");
+
+                    b.Property<string>("NameToken")
+                        .IsRequired()
+                        .HasColumnType("nvarchar(max)");
+
+                    b.HasKey("CategoryId");
+
+                    b.HasIndex("BookId")
+                        .IsUnique();
+
+                    b.ToTable("Category");
+                });
+
+            modelBuilder.Entity("BookTrackingApp.Models.CategoryType", b =>
+                {
                     b.Property<int>("Id")
                         .ValueGeneratedOnAdd()
                         .HasColumnType("int");
 
                     SqlServerPropertyBuilderExtensions.UseIdentityColumn(b.Property<int>("Id"), 1L, 1);
 
-                    b.Property<string>("BookAuthor")
+                    b.Property<string>("Name")
                         .IsRequired()
                         .HasColumnType("nvarchar(max)");
 
-                    b.Property<int>("BookRating")
-                        .HasColumnType("int");
-
-                    b.Property<string>("BookTitle")
+                    b.Property<string>("Type")
                         .IsRequired()
                         .HasColumnType("nvarchar(max)");
 
                     b.HasKey("Id");
 
-                    b.ToTable("Book");
+                    b.ToTable("CategoryType");
+                });
+
+            modelBuilder.Entity("BookTrackingApp.Models.Category", b =>
+                {
+                    b.HasOne("BookTrackingApp.Models.Book", "Book")
+                        .WithOne("Category")
+                        .HasForeignKey("BookTrackingApp.Models.Category", "BookId")
+                        .OnDelete(DeleteBehavior.Cascade)
+                        .IsRequired();
+
+                    b.Navigation("Book");
+                });
+
+            modelBuilder.Entity("BookTrackingApp.Models.Book", b =>
+                {
+                    b.Navigation("Category")
+                        .IsRequired();
                 });
 #pragma warning restore 612, 618
         }
